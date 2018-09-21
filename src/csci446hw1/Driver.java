db8d4b5f2327734ffb118f90d07f9335ff7cf12a
Karl Molina <karlgmolina@gmail.com>
2018-09-20 18:08:06 -0600

    initial commit

---
+++ src/csci446hw1/Driver.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csci446hw1;
+
+/**
+ * CSCI 446 Artificial Intelligence
+ * Homework 1
+ * Pacman Search
+ * @author Karl Molina, Jordan Palmer
+ */
+public class Driver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
